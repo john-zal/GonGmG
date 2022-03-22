41373a50c9ffbba9eba3950850c4221095d6c347
gongmg63 <82648436+gongmg63@users.noreply.github.com>
2022-03-22 09:35:36 +0900

    124나라

--- level2/124나라.java
+++ level2/124나라.java
@@ -3,13 +3,28 @@ package level2;
 class Solution {
     public String solution(int n) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
-        while(n > 3){
-            sb.append(n%3);
-            n = n/3;
+        int i = 3;
+        StringBuilder number = new StringBuilder();
+        StringBuilder minus = new StringBuilder();
+        while(n > 0){
+            n = n - i;
+            i *= 3;
+            number.append(3);
         }
-        
-        return answer;
+        n = Math.abs(n);
+        if(n==0)
+            minus.append(0);
+        else{
+            while(n != 0){
+                minus.append(n%3);
+                n /= 3;
+            }
+            minus = minus.reverse();
+        }
+        Long num = Long.parseLong(number.toString());
+        Long minu = Long.parseLong(minus.toString());
+
+        return answer = String.valueOf(num-minu).replaceAll("3", "4");
     }
 }
 public class 124나라 {
