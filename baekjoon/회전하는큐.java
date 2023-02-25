2b12d9eb14d4974246fe01dd4154f31a40efc9bf
gongmg63 <82648436+gongmg63@users.noreply.github.com>
2023-02-25 21:39:16 +0900

    02/25
    every day coding test - 2

--- baekjoon/회전하는큐.java
+++ baekjoon/회전하는큐.java
@@ -4,51 +4,54 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Iterator;
 
 public class 회전하는큐 {
+    static int right_circle(Deque<Integer> r_dq,int k) {
+        int answer = 0;
+        while(r_dq.peekFirst() != k) {
+            r_dq.addLast(r_dq.pollFirst());
+            ++answer;
+        }
+        r_dq.pollFirst();
+        return answer;
+    }
+    static int left_circle(Deque<Integer> l_dq, int k) {
+        int answer = 1;
+        while(l_dq.peekLast() != k) {
+            l_dq.addFirst(l_dq.pollLast());
+            ++answer;
+        }
+        l_dq.pollLast();
+        return answer;
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Deque<Integer> dq = new ArrayDeque<>();
-        Iterator<Integer> iter = dq.iterator();
-        Iterator<Integer> riter = dq.descendingIterator();
         
         String[] num = br.readLine().split(" ");
         String[] poplist = br.readLine().split(" ");
         int answer = 0;
-        for(int i = 1; i <= Integer.parseInt(num[0]); i++){
+        for(int i = 1; i <= Integer.parseInt(num[0]); i++)
             dq.add(i);
-        }
-        for(int i = 0; i < poplist.length; i++){
-            int popnum = Integer.parseInt(poplist[i]);
-            while(true){
-                if(popnum==dq.peekFirst()){
+        
+        for(String i : poplist) {
+            int k = Integer.parseInt(i);
+            while(true) {
+                if(dq.peekFirst() == k) 
                     dq.pollFirst();
-                    break;
-                }
+                
                 else {
-                    int first = 0;
-                    int firit = iter.next();
-                    int last = 0;
-                    int lastrit = riter.next();
-                    while(popnum != firit){
-                        first++;
-                        firit = iter.next();
+                    Deque<Integer> l_dq = new ArrayDeque<>(dq);
+                    Deque<Integer> r_dq = new ArrayDeque<>(dq);
+                    if(right_circle(r_dq,k) < left_circle(l_dq,k)) {
+                        answer += right_circle(dq, k);
                     }
-                    while(popnum != lastrit){
-                        last++;
-                        lastrit = riter.next();
-                    }
-                    if(first < last){
-                        dq.addLast(dq.pollFirst());
-                        answer++;
-                    }
-                    else if(first > last){
-                        dq.addFirst(dq.pollLast());
-                        answer++;
-                    }
+                    
+                    else 
+                        answer += left_circle(dq, k);
                 }
+                break;
             }
         }
         bw.write(String.valueOf(answer));
